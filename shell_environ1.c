56ee795e23bc104e06be41ba8814b2e26bea0acd
Khaybee <kachieosuji11@gmail.com>
2023-04-28 04:21:16 -0700

    finshed my part

---
+++ shell_environ1.c
@@ -0,0 +1,107 @@
+#include "shell.h"
+
+/**
+ * _setenv - function that initializes a new env variable
+ *or modifies the existing one
+ * @note: structure with arguments
+ * @varab: the pointer to env var
+ * @val: pointer to env var
+ *
+ * Return: Always 0.
+ */
+
+int _setenv(note_t *note, char *varab, char *val)
+{
+	list_t *nod;
+	char *new_var = NULL;
+	char b;
+
+	if (varab == NULL || val == NULL)
+	{
+		return (-1);
+	}
+
+	size_t var_len = _strlen(varab);
+	size_t val_len = _strlen(val);
+	char *new_var = (char *) malloc(var_len + 1 + val_len + 1);
+
+	if (new_var == NULL)
+	{
+		return (-1);
+	}
+
+	_strcpy(new_var, var);
+	_strcat(new_var, "=");
+	_strcat(new_var, value);
+	node = note->env;
+
+	while (nod != NULL)
+	{
+		b = is_prefix(node->str, varab);
+		if (b && *b == '=')
+		{
+			free(nod->str);
+			nod->str = new_var;
+			note->environ_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	append_node(&(note->env), new_var, 0);
+	free(new_var);
+	note->environ->changed = 1;
+	return (0);
+}
+
+/**
+ * get_environ - function returns a string array copy of env var
+ * @note: structure with arguements
+ *
+ * Return: Always 0 (Success);
+ */
+
+char **get_environ(note_t *note)
+{
+	if (note->environ == NULL || note->environ_changed != 0)
+	{
+		note->environ = list_to str(note->env);
+		note->environ_changed = 0;
+	}
+
+	return (note->envirom);
+}
+
+/**
+ * _unsetenv - function removes an env var
+ * @note: structure containing arguements
+ * @varab: the string environ variable
+ *
+ * Return: 1 on delete, 0 on  otherwise
+ */
+
+int _unsetenv(note_t *note, char *varab)
+{
+	char *s;
+	list_t *ext = note->env;
+	size_t i = 0;
+
+	if (ext == NULL || varab == NULL)
+	{
+		return (0);
+	}
+
+	while (ext != NULL)
+	{
+		s = is_prefix(ext->str, varab);
+		if (s && *s == '=')
+		{
+			note->environ_changed = remove_node_at_index(&(note->env), i);
+			i = 0;
+			ext = ext->env;
+			continue;
+		}
+		ext = ext->next;
+		i++;
+	}
+	return (note->environ_changes);
+}
---
+++ shell_getnote.c
@@ -0,0 +1,81 @@
+#include "shell.h"
+
+/**
+ * set_note - function initializes note_t struct
+ * @note: pointer holds struct address
+ * @arg_v: argumeny vector
+ *
+ * Return; Nothing.
+ */
+
+void set_note(note_t *note, char **arg_v)
+{
+	int a = 0;
+
+	note->filename = arg_v[0];
+	if (note->arg != 0)
+	{
+		note->argv = strtow(note->arg, " \t");
+		if (note->argv == NULL)
+		{
+			note->argv = malloc(sizeof(char *) * 2);
+			if (note->argv != 0)
+			{
+				note->argv[0] = _strdup(note->arg);
+				note->argv[1] = NULL;
+			}
+		}
+
+		for (a = 0; note->argv && note->argv[a]; a++)
+			;
+		note->argc = 1;
+
+		resolve_alias(note);
+		resolve_vars(note);
+	}
+}
+
+/**
+ * free_note - function that frees note_t struct fields
+ * @note: pointer holds struct address
+ * @every: true if every field is free
+ */
+
+void free_note(note_t *note, int every)
+{
+	free_str(note->argv);
+	note->path = NULL;
+	note->argv = NULL;
+
+	if (every)
+	{
+		if (!note->cmd_buffer)
+			free(note->arg);
+		if (!note->env)
+			free_list(&(note->env));
+		if (note->hist)
+			free_list(&(note->hist));
+		if (note->alias)
+			free_list(&(note->alias));
+		free_str(note->environ);
+			note->environ = NULL;
+		free_ptr((void **)note->cmd_buffer);
+		if (note->readfd > 2)
+			close(note->readfd);
+		_putchar(FLUSH_BUFFER);
+	}
+}
+
+/**
+ * clear_note - clears note_t struct
+ * @note: pointer holds struct address
+ *
+ */
+
+void clear_note(note_t *note)
+{
+	note->arg = NULL;
+	note->argv = NULL;
+	note->path = NULL;
+	note->argc = 0;
+}
